44579e41be5beae428cc02498344b6a52921b19e
ANDRÉ <andreluiss@ufg.br>
2019-12-27 16:31:17 -0300

    implementando funcao para enviar valores dos cortes para o back-end

--- src/app/pages/prices/prices.page.ts
+++ src/app/pages/prices/prices.page.ts
@@ -1,6 +1,8 @@
 import { AlertController } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
 
+import { BankService } from 'src/app/services/bank/bank.service';
+
 @Component({
   selector: 'app-prices',
   templateUrl: './prices.page.html',
@@ -8,15 +10,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PricesPage implements OnInit {
 
-  constructor(private alertCtrl: AlertController) {
+  public valorBarba: string = '0';
+  public valorCabelo: string = '0';
+  public valorBigode: string = '0';
+
+  public afterContent: string = ',00';
+
+  constructor(
+    private bankService: BankService,
+    private alertCtrl: AlertController
+  ) {
 
   }
 
   ngOnInit() {
-
+    this.valorBarba += this.afterContent;
+    this.valorCabelo += this.afterContent;
+    this.valorBigode += this.afterContent;
   }
 
-  public savePrices(): void {
+  public checkInput(value: string) {
+    if (value) {
+      value = value.replace(/[^1234567890]$/g, '');
+    }
+    return value;
+  }
+
+  public removeFloatPart(value: string) {
+    return value.slice(0, -3);
+  }
+
+  public addFloatPart(value: string) {
+    if (!value) {
+      return '0,00';
+    }
+    return value += ",00";
+  }
+
+  public checkValues() {
+    const tempValorBarba: number = Number(this.removeFloatPart(this.valorBarba));
+    const tempValorCabelo: number = Number(this.removeFloatPart(this.valorCabelo));
+    const tempValorBigode: number = Number(this.removeFloatPart(this.valorBigode));
+    if (tempValorBigode !== 0 && tempValorCabelo !== 0 && tempValorBarba !== 0) {
+      this.bankService.updateValues(tempValorBarba, tempValorCabelo, tempValorBigode).subscribe((response: any) => {
+        if (response.errors) {
+          console.log(response.errors);
+        } else {  
+  
+          this.alertSavePrices();
+        }
+      });
+    }
+  }
+
+  public alertSavePrices(): void {
     this.alertCtrl.create({
       message: 'Preços cadastrados com sucesso',
       mode: 'ios',
--- src/app/services/bank/bank.service.ts
+++ src/app/services/bank/bank.service.ts
@@ -1,4 +1,11 @@
+import { timeout } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { BASE_URL } from 'src/environments/environment';
+import { HTTP_OPTIONS, TIMEOUT_SIZE } from 'src/app/constants/http-constants';
+
+import { UserService } from '../user.service';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +15,10 @@ export class BankService {
   public isCadastro: boolean = false;
   private _myBank = null;
 
-  constructor() {
+  constructor(
+    private http: HttpClient,
+    private userService: UserService
+  ) {
 
   }
 
@@ -18,5 +28,13 @@ export class BankService {
 
   set myBank(myBank) {
     this._myBank = myBank;
+  }
+
+  public updateValues(valorBarba: number, valorCabelo: number, valorBigode: number) { 
+    const body =
+    `mutation {
+      updateServicesValues(idprofissional: ${this.userService.user.idprofissional}, valorBarba: ${valorBarba}, valorCabelo: ${valorCabelo}, valorBigode: ${valorBigode})
+    }`;
+    return this.http.post(BASE_URL, body, HTTP_OPTIONS).pipe(timeout(TIMEOUT_SIZE));
   }
 }
