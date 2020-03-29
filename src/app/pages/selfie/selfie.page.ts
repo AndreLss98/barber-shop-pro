875a4de6e48006ebb19449990e6ce19e66bb85c6
AndreLss <andre_luis98@outlook.com>
2020-03-29 19:15:59 -0300

    corrigindo fluxo de envio de fotos para o back-end

--- src/app/pages/selfie/selfie.page.ts
+++ src/app/pages/selfie/selfie.page.ts
@@ -1,10 +1,11 @@
 import { Router } from '@angular/router';
-import { ModalController, LoadingController } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
+import { ModalController, LoadingController, AlertController } from '@ionic/angular';
 
 import { CameraOptions, Camera } from '@ionic-native/camera/ngx';
 
 import { CadastroSucessoComponent } from '../modals/cadastro-sucesso/cadastro-sucesso.component';
+import { ConnectionStatusComponent } from '../modals/connection-status/connection-status.component';
 
 import { UserService } from 'src/app/services/user.service';
 
@@ -21,8 +22,9 @@ export class SelfiePage implements OnInit {
     private route: Router,
     private camera: Camera,
     private userService: UserService,
+    private alertCtrl: AlertController,
     private modalCtrl: ModalController,
-    private loadingCtrl: LoadingController
+    private loadingCtrl: LoadingController,
   ) {
 
   }
@@ -49,33 +51,16 @@ export class SelfiePage implements OnInit {
     this.userService.sendRegister().subscribe((response: any) => {
       if (response.errors) {
         this.closeLoading();
+        this.connectionError();
         console.error(response.errors);
       } else {
         const idprofissional = response.data.registerProfissional.idprofissional;
-        const { imgDocFront, imgDocBack, selfie } = this.userService.newUser;
-        this.userService.uploadImg(imgDocFront, 'imgDocFront', idprofissional).subscribe((response: any) => {
-          this.userService.uploadImg(imgDocBack, 'imgDocBack', idprofissional).subscribe((response: any) => {
-            this.userService.uploadImg(selfie, 'imgSelfie', idprofissional).subscribe((response: any) => {
-              this.closeLoading();
-              this.modalCtrl.create({ component: CadastroSucessoComponent }).then((modal) => {
-                modal.present();
-                modal.onWillDismiss().then(() => this.route.navigateByUrl('login'));
-              });
-            }, (error) => {
-              this.closeLoading();
-              console.error('Error ao enviar selfie: ', error);  
-            })
-          }, (error) => {
-            this.closeLoading();
-            console.error('Error ao enviar img da parte de trás do documento: ', error);
-          });
-        }, (error) => {
-          this.closeLoading();
-          console.error('Error ao enviar img da frente do documento: ', error);
-        });
+        this.sendAllPictures(idprofissional, this.userService.newUser);
       }
     }, (error) => {
       this.closeLoading();
+      this.connectionError();
+      console.log('Sub error: ', error);
     });
   }
 
@@ -88,8 +73,63 @@ export class SelfiePage implements OnInit {
     })
   }
 
-  public async closeLoading() {
-    await this.loadingCtrl.dismiss();
+  public closeLoading() {
+    setTimeout(() => {
+      this.loadingCtrl.dismiss();
+    }, 500);
+  }
+
+  private connectionError(): void {
+    setTimeout(() => {
+      this.modalCtrl.create({ component: ConnectionStatusComponent }).then((modal) => modal.present());
+    }, 650);
+  }
+
+  private sendAllPictures(idprofissional, user) {
+    const { imgDocFront, imgDocBack, selfie } = user;
+    this.userService.uploadImg(imgDocFront, 'imgDocFront', idprofissional).subscribe((response: any) => {
+      this.userService.uploadImg(imgDocBack, 'imgDocBack', idprofissional).subscribe((response: any) => {
+        this.userService.uploadImg(selfie, 'imgSelfie', idprofissional).subscribe((response: any) => {
+          this.closeLoading();
+          this.modalCtrl.create({ component: CadastroSucessoComponent }).then((modal) => {
+            modal.present();
+            modal.onWillDismiss().then(() => this.route.navigateByUrl('login'));
+          });
+        }, (error) => {
+          this.closeLoading();
+          console.error('Error ao enviar selfie: ', error);
+          this.showAlert(idprofissional, user);
+        })
+      }, (error) => {
+        this.closeLoading();
+        console.error('Error ao enviar img da parte de trás do documento: ', error);
+        this.showAlert(idprofissional, user);
+      });
+    }, (error) => {
+      this.closeLoading();
+      console.error('Error ao enviar img da frente do documento: ', error);
+      this.showAlert(idprofissional, user);
+    });
+  }
+
+  private showAlert(idprofissional, user) {
+    this.alertCtrl.create({
+      message: 'Erro ao enviar imagens para o servidor',
+      mode: 'ios',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'Cancel'
+        },
+        {
+          text: 'Tentar Novamente',
+          handler: () => {
+            this.showLoading();
+            this.sendAllPictures(idprofissional, user);
+          }
+        }
+      ]
+    }).then((alert) => alert.present());
   }
 
 }
